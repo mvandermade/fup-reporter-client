185afd6185e36276de23f09b0585b146f7bf6975
martynem <204603850+martynem@users.noreply.github.com>
2025-08-16 13:55:45 +0200

    Use asyncPipe for visualizing postzegel codes

--- src/app/routes/postzegel-visualizer-list/postzegel-visualizer-list.ts
+++ src/app/routes/postzegel-visualizer-list/postzegel-visualizer-list.ts
@@ -1,40 +1,36 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
-import {webSocket} from "rxjs/webSocket";
-import {isWebSocketSerialEvent, WebSocket_SERIAL_EVENT, WebSocketBaseType} from "./websocket-types";
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AsyncPipe, NgForOf} from "@angular/common";
-import {map, Observable, of, startWith, Subscription} from "rxjs";
+import {BehaviorSubject, Subscription} from "rxjs";
 import {WebSocketService} from "../../connectors/websocket/WebSocketService";
+import {isWebSocketSerialEvent} from "./websocket-types";
 
 @Component({
   selector: 'app-postzegel-visualizer',
-    imports: [AsyncPipe, NgForOf],
+  imports: [AsyncPipe],
   templateUrl: './postzegel-visualizer-list.html',
   styleUrl: './postzegel-visualizer-list.scss',
 })
-export class PostzegelVisualizerList implements OnDestroy, OnInit{
-  tiles: string[] = []
-  subscriptions = new Subscription();
+export class PostzegelVisualizerList implements OnDestroy, OnInit {
+  private tilesSubject = new BehaviorSubject<string[]>([]);
+  tiles$ = this.tilesSubject.asObservable();
+  
+  private subscriptions = new Subscription();
+
+  constructor(private webSocketService: WebSocketService) {}
 
   ngOnInit() {
-      const tilesSubscription = this.webSocketService
-          .getObservable()
-          .subscribe(message => {
-              if (isWebSocketSerialEvent(message)) {
-                  this.tiles = [...this.tiles, message.code]
-                  this.changeDetection.markForCheck()
-              }
-          })
-      this.subscriptions.add(tilesSubscription);
+    const tilesSubscription = this.webSocketService
+      .getObservable()
+      .subscribe(message => {
+        if (isWebSocketSerialEvent(message)) {
+          const currentTiles = this.tilesSubject.value;
+          this.tilesSubject.next([...currentTiles, message.code]);
+        }
+      });
+    this.subscriptions.add(tilesSubscription);
   }
 
   ngOnDestroy() {
-      this.subscriptions.unsubscribe();
-  }
-
-  constructor(
-      private webSocketService: WebSocketService,
-      private changeDetection: ChangeDetectorRef,
-  ) {
-
+    this.subscriptions.unsubscribe();
   }
 }
